c034a0cff426188744d3394c1a6c8995c8fce131
James Pan <james.pan@unswengsoc.com>
2022-07-23 00:34:16 +1000

    frontend: blank out new components

--- frontend/src/routes/tools.tsx
+++ frontend/src/routes/tools.tsx
@@ -12,33 +12,33 @@ export const Tools = memo(() => {
   const [subtopicFilters, setSubtopicFilters] = useState<string[]>([]);
   const navigate = useNavigate();
   return (
-    <div className="md:px-24">
+    <div className='md:px-24'>
       <SearchBar
         onClick={(route) => {
           navigate(`/tools/${route}`);
         }}
         filters={categoryFilters?.concat(subtopicFilters)}
       />
-      <div className="flex flex-col md:flex-row md:space-x-4 py-4">
-        <div className="md:w-56">
+      <div className='flex flex-col md:flex-row md:space-x-4 py-4'>
+        <div className='md:w-56'>
           <Dropdown
-            label="Category"
-            placeholder="Pain, musculoskeletal..."
+            label='Category'
+            placeholder='Pain, musculoskeletal...'
             values={enumToStringArray(AssessmentNS.Category)}
             onPress={(values) => setCategoryFilters(values)}
           />
         </div>
-        <div className="md:w-56">
+        <div className='md:w-56'>
           <Dropdown
-            label="Subtopic"
-            placeholder="Quantification..."
+            label='Subtopic'
+            placeholder='Quantification...'
             values={enumToStringArray(AssessmentNS.SubTopic)}
             onPress={(values) => setSubtopicFilters(values)}
           />
         </div>
       </div>
-      <RadioButtons inputs={['A', 'B', 'C', 'D']} />
-      <CheckBoxes inputs={['A', 'B', 'C', 'D']} />
+      {/* <RadioButtons inputs={['A', 'B', 'C', 'D']} />
+      <CheckBoxes inputs={['A', 'B', 'C', 'D']} /> */}
     </div>
   );
 });
